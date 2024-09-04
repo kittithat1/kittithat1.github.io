c6530c16eaf2357e188bd783fac6ae43e4b245b7
kittithat1 <kittitkks@gmail.com>
2024-09-04 09:04:10 +0700

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,25 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="tel:11122233333"><i class="fas fa-phone"></i> +111 222 3333</a>
+         <a href="mailto:shaikh@gmail.com"><i class="fas fa-envelope"></i> 65202040047@minburi.ac.th</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> minburi 123 </a>
+      </div>
+
+   
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>DeawScents</span> | all rights reserved!</div>
+
+</footer>
